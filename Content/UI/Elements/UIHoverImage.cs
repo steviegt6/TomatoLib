e75ad899428ffe43e30c7f96b3c798d30d3045d0
Stevie <xxlennygamerxx@gmail.com>
2021-08-06 20:19:58 -0700

    Release 1.0.1!

---
+++ Content/UI/Elements/UIHoverImage.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+using Terraria.ModLoader.UI;
+
+namespace TomatoLib.Content.UI.Elements
+{
+    public class UIHoverImage : UIImage
+    {
+        public string HoverText { get; }
+
+        public UIHoverImage(Asset<Texture2D> texture, string hoverText) : base(texture)
+        {
+            HoverText = hoverText;
+        }
+
+        protected override void DrawSelf(SpriteBatch spriteBatch)
+        {
+            base.DrawSelf(spriteBatch);
+
+            if (!IsMouseHovering)
+                return;
+
+            Rectangle parentDims = Parent.GetDimensions().ToRectangle();
+            parentDims.Y = 0;
+            parentDims.Height = Main.screenHeight;
+
+            UICommon.DrawHoverStringInBounds(spriteBatch, HoverText, parentDims);
+        }
+    }
+}
