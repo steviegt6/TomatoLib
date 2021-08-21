d74e8febb1aa74c967f24bf7f4b091fa9bd1e2de
Stevie <xxlennygamerxx@gmail.com>
2021-08-21 16:51:30 -0700

    Clean-up post-merge.

--- TomatoMod.cs
+++ TomatoMod.cs
@@ -1,21 +1,37 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using log4net;
 using MonoMod.RuntimeDetour;
 using MonoMod.RuntimeDetour.HookGen;
 using Terraria.ModLoader;
 using TomatoLib.Core.Compatibility.Calls;
 using TomatoLib.Core.Drawing;
+using TomatoLib.Core.Localization;
 using TomatoLib.Core.Logging;
+using TomatoLib.Core.MonoModding;
 using TomatoLib.Core.Reflection;
 using TomatoLib.Core.Utilities.Compatibility.Calls;
+using TomatoLib.Core.Utilities.Extensions;
+using TomatoLib.Core.Utilities.Localization;
 using TomatoLib.Core.Utilities.Logging;
 
 namespace TomatoLib
 {
+    /// <summary>
+    ///     Extends <see cref="Mod"/>'s capabilities.
+    /// </summary>
     public class TomatoMod : Mod
     {
-        public IModLogger ModLogger { get; protected set; }
+        /// <summary>
+        ///     Extended-capability <see cref="ILog"/>.
+        /// </summary>
+        public virtual IModLogger ModLogger { get; protected set; }
+
+        /// <summary>
+        ///     Handles localization loading for your mod.
+        /// </summary>
+        public virtual ILocalizationLoader LocalizationLoader { get; } = new DefaultLocalizationLoader();
 
         public virtual IModCaller CallHandler { get; protected set; } = new DefaultModCaller();
 
@@ -39,11 +55,19 @@ namespace TomatoLib
 
             // This is a temporary fix for some methods in HookHelper.
             ExecutePrivately(MonoModHooks.RequestNativeAccess);
+
+            ExecutePrivately(() =>
+            {
+                this.CreateDetour(typeof(LocalizationLoader).GetCachedMethod("Autoload"),
+                    GetType().GetCachedMethod(nameof(AutoLoadLocalization)));
+            });
         }
 
         public override void Unload()
         {
             base.Unload();
+
+            LocalizationLoader.Unload();
 
             foreach ((MethodInfo method, Delegate callback) in DelegatesToRemove)
                 HookEndpointManager.Unmodify(method, callback);
@@ -64,6 +88,14 @@ namespace TomatoLib
         }
 
         public override object Call(params object[] args) => CallHandler.HandleCall(this, args);
+
+        private static void AutoLoadLocalization(Action<Mod> orig, Mod mod)
+        {
+            orig(mod);
+
+            if (mod is TomatoMod tomatoMod)
+                tomatoMod.LocalizationLoader?.Load(mod);
+        }
 
         private void ExecutePrivately(Action action)
         {
