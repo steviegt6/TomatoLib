75ac6982b9b9c5946459985271cb08945981df4a
Stevie <xxlennygamerxx@gmail.com>
2021-08-30 15:51:35 -0700

    Add GLCallLocker.

---
+++ Core/Threading/GLCallLocker.cs
@@ -0,0 +1,81 @@
+﻿#region License
+// Copyright (C) 2021 Tomat and Contributors
+// GNU General Public License Version 3, 29 June 2007
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.Xna.Framework;
+using Terraria;
+using TomatoLib.Common.Systems;
+
+namespace TomatoLib.Core.Threading
+{
+    public sealed class GLCallLocker : SingletonSystem<GLCallLocker>
+    {
+        private int? MainThreadId;
+        [CanBeNull] private List<Action> Actions;
+        [CanBeNull] private LockProvider Lock;
+
+        public override void Load()
+        {
+            base.Load();
+
+            Actions = new List<Action>();
+
+            On.Terraria.Main.Update += CallActionsOnMainThread;
+        }
+
+        private void CallActionsOnMainThread(On.Terraria.Main.orig_Update orig, Main self, GameTime gameTime)
+        {
+            // Create lock on main thread
+            Lock ??= new LockProvider();
+            MainThreadId ??= Thread.CurrentThread.ManagedThreadId;
+
+            if (Actions != null)
+            {
+                foreach (Action action in Actions)
+                    action?.Invoke();
+
+                Actions.Clear();
+            }
+
+            orig(self, gameTime);
+        }
+
+        public async Task<T> InvokeAsync<T>(Func<T> task)
+        {
+            TaskCompletionSource<T> completionSource = new();
+
+            if (MainThreadId.HasValue && Thread.CurrentThread.ManagedThreadId == MainThreadId.Value)
+            {
+                completionSource.SetResult(task());
+                return await completionSource.Task;
+            }
+
+            if (Lock == null)
+                throw new Exception("Lock object was null when attempting to invoke function in GLCallLocker.");
+
+            object lockObject = Lock.LockObject;
+            lock (lockObject)
+            {
+                Actions?.Add(() =>
+                {
+                    try
+                    {
+                        completionSource.SetResult(task());
+                    }
+                    catch (Exception e)
+                    {
+                        completionSource.SetException(e);
+                    }
+                });
+            }
+
+            return await completionSource.Task;
+        }
+    }
+}
---
+++ Core/Threading/LockProvider.cs
@@ -0,0 +1,12 @@
+﻿#region License
+// Copyright (C) 2021 Tomat and Contributors
+// GNU General Public License Version 3, 29 June 2007
+#endregion
+
+namespace TomatoLib.Core.Threading
+{
+    public sealed class LockProvider
+    {
+        public readonly object LockObject = new();
+    }
+}
